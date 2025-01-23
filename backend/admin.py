0fd1eb199e9560e6a3017eec4ea8688c586084c9
[200~essadikine.fst@uhp.ac.ma <essadikine.fst@uhp.ac.ma>
2025-01-23 21:46:59 +0100

    changer le contenu de admin.py avec langue français at commenter les lignes

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Uploaded_File
 
-# Register your models here.
-admin.site.register(Uploaded_File)
+# Enregistrement de votre modèle ici dans l'interface d'administration Django
+# Cela permet de gérer les fichiers téléchargés via l'interface admin.
+admin.site.register(Uploaded_File)  # Enregistre le modèle Uploaded_File pour le rendre disponible dans l'admin
