c55ac0fdc9744c37ae27e4bade2a92ae151c41d5
[200~essadikine.fst@uhp.ac.ma <essadikine.fst@uhp.ac.ma>
2025-01-23 21:45:41 +0100

    changer le contenu de modelss.py avec langue français at commenter les lignes

--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 
-# Create your models here.
-
-
+# Modèle pour les fichiers téléchargés
 class Uploaded_File(models.Model):
-    file_id = models.CharField(max_length=255,blank=True)
-    file_name = models.CharField(max_length=255,blank=True)
-    file_format = models.CharField(max_length=255,blank=True)
-    file = models.FileField(upload_to=f'uploads',blank=True,)
-    file_date  = models.DateTimeField(auto_now=True,null=True,blank=True)
-    
-    
+    file_id = models.CharField(max_length=255, blank=True)  # ID unique du fichier
+    file_name = models.CharField(max_length=255, blank=True)  # Nom du fichier
+    file_format = models.CharField(max_length=255, blank=True)  # Format du fichier
+    file = models.FileField(upload_to='uploads', blank=True)  # Chemin du fichier téléchargé
+    file_date = models.DateTimeField(auto_now=True, null=True, blank=True)  # Date de modification
