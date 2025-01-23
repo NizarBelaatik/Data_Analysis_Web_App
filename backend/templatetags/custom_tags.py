0d37e66aba1f857d1fcf3f6abb540ecadeb898be
[200~essadikine.fst@uhp.ac.ma <essadikine.fst@uhp.ac.ma>
2025-01-23 21:49:51 +0100

    up to date

--- backend/templatetags/custom_tags.py
+++ backend/templatetags/custom_tags.py
@@ -1,8 +1,9 @@
 from django import template
 
+# Création d'une bibliothèque de balises pour les filtres personnalisés
 register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
-    """Custom filter to access dictionary values by key"""
+    """Filtre personnalisé pour accéder aux valeurs d'un dictionnaire par clé"""
     return dictionary.get(key)
